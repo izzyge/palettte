26053424f6c64a4160b4ab1a6ffc9665661ddb77
dianalimnos <dianalim1117@gmail.com>
2018-07-17 14:08:17 -0700

    Added some files for routes.

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {type: String, required: true},
+  color1: {type: String, required: true},
+  color2: {type: String, required: true},
+  color3: {type: String, required: true},
+  color4: {type: String, required: true},
+});
+
+module.exports = mongoose.model('Post', PostSchema);
