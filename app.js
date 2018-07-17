bdcb014e5acc41634688b6e4107e8a93f4533a50
natalielim <natalielim@gmail.com>
2018-07-17 09:32:14 -0700

    Connected to MongoDB

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const mongoose = require('mongoose');
+const mongoURI = '(your mongodb URI)';
+
+mongoose.connect(mongoURI)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var app = express();
 
 // view engine setup
