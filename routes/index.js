702fa6a99810492062398929600ec7318ec3a210
dianalimnos <dianalim1117@gmail.com>
2018-07-17 15:25:44 -0700

    Connected to mlab
    Can fill out new post form, data will save in mlab.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const Post = require('../models/post');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PALETTTE' });
+router.get('/', (req, res) => {
+  Post.find({}, (err, reviews) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.render('posts/index', {
+      // posts: posts
+    })
+  });
 });
+
+router.get('/posts/new', (req, res) => {
+  res.render('posts/new');
+});
+
+router.post('/posts', (req, res) => {
+  const post = new Post(req.body);
+
+  post.save(function(err, review) {
+    if (err) {
+      console.log(err);
+    }
+
+    return res.redirect('/posts/' + post._id);
+  });
+});
+
 
 module.exports = router;
